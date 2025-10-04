9981539cf6594a71c2c9425c40f34869a2b1eb52
saisannihithch <saiandrkco@gmail.com>
2025-10-04 01:35:30 -0400

    fix hooks and tsconfig

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -1,22 +1,19 @@
-import { useState, useEffect } from 'react';
+import * as React from "react"
+
+const MOBILE_BREAKPOINT = 768
 
 export function useIsMobile() {
-  const [isMobile, setIsMobile] = useState(false);
+  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
 
-  useEffect(() => {
-    const checkIsMobile = () => {
-      setIsMobile(window.innerWidth < 768);
-    };
+  React.useEffect(() => {
+    const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
+    const onChange = () => {
+      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    }
+    mql.addEventListener("change", onChange)
+    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
+    return () => mql.removeEventListener("change", onChange)
+  }, [])
 
-    // Check on initial render
-    checkIsMobile();
-
-    // Add event listener for window resize
-    window.addEventListener('resize', checkIsMobile);
-
-    // Cleanup event listener
-    return () => window.removeEventListener('resize', checkIsMobile);
-  }, []);
-
-  return isMobile;
+  return !!isMobile
 }
